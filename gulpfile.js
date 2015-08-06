3ac2f31a59f4ed30bff8bfafca69f4f942c86fdf
julien.manganne <julien.manganne@extreme-sensio.com>
2015-08-07 01:03:08 +0200

    [WIP] Added default gulp tasks for web assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-var gulp       = require('gulp');
-var stylus     = require('gulp-stylus');
-var concat     = require('gulp-concat');
-var uglify     = require('gulp-uglify');
-// var uglify     = require('gulp-csso');
-var del        = require('del');
+var gulp   = require('gulp');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var csso   = require('gulp-csso');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var del    = require('del');
 
 var paths = {
-  custom_scripts: 'assets/js/*.js',
-  vendor_scripts: 'assets/js/vendor/*.js',
-  custom_styles: 'assets/css/**/*.css'
+  scripts: ['src/js/lib/*.js', 'src/js/*.js'],
+  styles: 'src/stylus/*.styl'
 };
 
 gulp.task('clean', function(cb) {
@@ -17,27 +18,34 @@ gulp.task('clean', function(cb) {
 });
 
 // Concatenate & Minify JS
-gulp.task('compress_scripts', function() {
-    return gulp.src(paths.custom_scripts)
-        .pipe(uglify())
-        .pipe(gulp.dest('./assets/js'));
-});
-
-gulp.task('concat_scripts', function() {
-    return gulp.src('./assets/js/*.js')
+gulp.task('scripts', function() {
+    return gulp.src(paths.scripts)
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        // .pipe(notify({ message: 'Scripts task complete' }))
+    ;
 });
 
 gulp.task('styles', function() {
-
+    return  gulp.src(paths.styles)
+        .pipe(stylus())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(csso())
+        .pipe(gulp.dest('dist/css'))
+    ;
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.styles, ['css']);
+  gulp.watch(paths.scripts, ['compress_scripts', 'concat_scripts']);
+  gulp.watch(paths.styles, ['styles']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'compress_scripts', 'concat_scripts', 'styles']);
+gulp.task('default', ['watch', 'scripts', 'styles']);
