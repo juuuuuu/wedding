f67da9b2fed8b5decf1121e904201f8e283a80e6
julien.manganne <julien.manganne@extreme-sensio.com>
2015-08-12 23:31:48 +0200

    [Gulp] Fixed dist image

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,46 @@ var del    = require('del');
 
 var paths = {
   scripts: 'src/js/*.js',
-  styles: 'src/stylus/*.styl'
+  styles: 'src/stylus/*.styl',
+  images: 'src/images/*',
 };
 
 gulp.task('clean', function(cb) {
-    del(['dist'], cb)
+  del(['dist/**'], cb);
 });
 
 gulp.task('scripts', function() {
-    return gulp.src(paths.scripts)
-        .pipe(concat('all.js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'))
-    ;
+  return gulp.src(paths.scripts)
+    .pipe(concat('all.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+  ;
 });
 
 gulp.task('styles', function() {
-    return  gulp.src(paths.styles)
-        .pipe(stylus())
-        .pipe(concat('all.css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(csso())
-        .pipe(gulp.dest('dist'))
-    ;
+  return  gulp.src(paths.styles)
+    .pipe(stylus())
+    .pipe(concat('all.css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(csso())
+    .pipe(gulp.dest('dist'))
+  ;
+});
+
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+    .pipe(gulp.dest('dist/images'))
+  ;
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.images, ['images']);
 });
 
 gulp.task('default', ['clean', 'watch'], function() {
-  gulp.start('scripts', 'styles');
+  gulp.start('scripts', 'styles', 'images');
 });
 
